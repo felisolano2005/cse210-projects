eaafeb115d495b5611d830294f8b8eb0717b1027
felisolano2005 <felisolano2005@gmail.com>
2026-03-14 17:22:56 -0600

    Learning Activities Done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade?");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+
+        if (number >= 90)
+        letter = "A";
+
+        else if (number >= 80)
+        letter = "B";
+
+        else if (number >= 70)
+        letter = "C";
+
+        else if (number >= 60)
+        letter = "D";
+
+        else
+        letter = "F";
+
+        Console.WriteLine($"You have earned the grade {letter}");
+
+        if (number >= 70)
+        Console.WriteLine("Congratulations, you passed the course");
+        else
+        Console.WriteLine("Enter a valid number!");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
