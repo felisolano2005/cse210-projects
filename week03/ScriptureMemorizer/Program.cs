3f6f4cd776d001d472d13f27fe9fcc005fb1be7e
felisolano2005 <felisolano2005@gmail.com>
2026-03-28 16:17:13 -0600

    Scripture Memorizing Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        List<Scripture> scriptures = new List<Scripture>
+        {
+            new Scripture(new Reference("1 Nephi", 3, 7),
+                "I will go and do the things which the Lord hath commanded"),
+            
+            new Scripture(new Reference("Mosiah", 2, 17),
+                "When ye are in the service of your fellow beings ye are only in the service of your God"),
+
+            new Scripture(new Reference("John", 3, 16),
+                "For God so loved the world that he gave his one and only Son")
+        };
+
+        Random rand = new Random();
+        Scripture scripture = scriptures[rand.Next(scriptures.Count)];
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.AllWordsHidden())
+            {
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit':");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _rand = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] parts = text.Split(" ");
+        foreach (string word in parts)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        int hidden = 0;
+
+        while (hidden < count)
+        {
+            int index = _rand.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return text;
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
